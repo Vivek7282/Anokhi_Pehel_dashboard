97572e35f50f03f4f563088085cf9637c87e0ee9
VIVEK KUMAR <kumarvivek7282827749@gmail.com>
2023-09-12 16:36:49 +0530

    Added vercel

--- Server/index.js
+++ Server/index.js
@@ -8,6 +8,22 @@ mongoDB();
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.use(express.static(path.resolve(__dirname, "client", "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "client", "build", "index.html"),
+      function (err) {
+        if (err) {
+          res.status(500).send(err);
+        }
+      }
+    );
+  });
+}
+
 app.use("/images", express.static("images"));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
